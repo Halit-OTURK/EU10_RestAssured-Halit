692f52c45f74e093023e0a34f8f0ff559ba1e74a
Halit Oturk <halithamza0920@gmail.com>
2023-03-31 21:58:41 +0300

    day3 finished

--- src/test/java/com/cybertek/Day_3/SpartanTestsWithParameters.java
+++ src/test/java/com/cybertek/Day_3/SpartanTestsWithParameters.java
@@ -2,17 +2,14 @@ package com.cybertek.Day_3;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class SpartanTestsWithParameters {
 
